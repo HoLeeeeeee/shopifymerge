350a4e120090c298d225522808ce20903d177ad1
Hour Li <1395923251@qq.com>
2024-04-22 10:35:52 +0800

    update

--- Desktop/mileseeytools_shopline/assets/theme-editor.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('shopline:section:unload', (event) => {
-  const { sectionId } = event.detail;
-
-  const productModal = document.querySelector(`#ProductModal-${sectionId}`);
-
-  if (productModal) {
-    productModal.remove();
-  }
-});
